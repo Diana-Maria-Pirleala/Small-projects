8ebaa2c453a90f69be17420583528a308bdc24ee
Diana-Maria Pîrleală <128043490+Diana-Maria-Pirleala@users.noreply.github.com>
2026-01-20 20:26:25 +0200

    FizzBuzz.js

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -2,13 +2,18 @@ var count = 1;
 var output = [];
 
 function fizzBuzz() {
+  
   while (count <= 100) {
+    
     if (count % 3 === 0 && count % 5 === 0) {
       output.push("FizzBuzz");
+      
     } else if (count % 3 === 0) {
       output.push("Fizz");
+      
     } else if (count % 5 === 0) {
       output.push("Buzz");
+      
     } else {
       output.push(count);
     }
@@ -17,3 +22,4 @@ function fizzBuzz() {
 
   console.log(output);
 }
+
